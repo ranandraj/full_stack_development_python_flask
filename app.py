485034e40d471463a9646ebf2a8b99a1da8564ae
ranandraj <ranandraj@gmail.com>
2026-03-11 12:14:32 +0530

    update

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="your_password",
+    database="admission_portal_db"
+)
 
+cursor = db.cursor()
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
+
+    if request.method == "POST":
+
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        phone = request.form['phone']
+
+        sql = """
+        INSERT INTO students(full_name,email,password,phone)
+        VALUES(%s,%s,%s,%s)
+        """
+
+        values = (name,email,password,phone)
+
+        cursor.execute(sql,values)
+
+        db.commit()
+
+        return redirect("/login")
+
     return render_template("register.html")
 
 @app.route("/login")
